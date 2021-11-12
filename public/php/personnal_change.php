5e2b17c26798c3b4337b5a01e62e6e8e319d38dd
BarthetLeo <93932005+BarthetLeo@users.noreply.github.com>
2021-11-12 15:59:13 +0100

    Oui

--- public/php/personnal_change.php
+++ public/php/personnal_change.php
@@ -10,52 +10,46 @@
 
     <body>
         <div id="Choice">
-            <table border="1">
-                <caption>Sommaire</caption>
-                <tr>
-                    <td><a href="#info_connexion" class="lien">Informations de connexion</a></td>
-                </tr>
-                <tr>
-                    <td><a href="#info_public" class="lien">Informations publiques</a></td>
-                </tr>
-                <tr>
-                    <td><a href="#info_perso" class="lien">Informations personnels</a></td>
-                </tr>
-                <tr>
-                    <td><a href="#supprimer" class="lien">Supprimer le compte</a></td>
-                </tr>
-            </table>
+            <title>Sommaire</title>
+            <ul>
+                <li><a href="#info_connexion" class="lien">Informations de connexion</a></li>
+                <li><a href="#info_public" class="lien">Informations publiques</a></li>
+                <li><a href="#info_perso" class="lien">Informations personnels</a></li>
+                <li><a href="#supprimer" class="lien">Supprimer le compte</a></li>
+            </ul>
         </div>
         
-        <div class="anchre" id="info_connexion">
-                Ici les infos de connexions
-        </div>
+        <div id="changement">
+            <div class="anchre" id="info_connexion">
+                    Ici les infos de connexions
+            </div>
 
-        <div class="anchre" id="info_public">
-            <p>
-                Ici les infos publiques
-            </p>
-        </div>
+            <div class="anchre" id="info_public">
+                <p>
+                    Ici les infos publiques
+                </p>
+            </div>
 
 
-        <div class="anchre" id="info_perso">
-                Ici les infos persos
-        </div>
+            <div class="anchre" id="info_perso">
+                    Ici les infos persos
+            </div>
 
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            
 
-        <div class="anchre" id="supprimer">
-            <p>
-                ici pour supprimer le compte
-            </p>
+            <div class="anchre" id="supprimer">
+                <p>
+                    ici pour supprimer le compte
+                </p>
+            </div>
         </div>
     </body>
 </html>

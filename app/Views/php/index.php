2757a06a5c2f0d86f27b7520b7c7f83857a62669
Emmanuel <55059322+Mabule@users.noreply.github.com>
2022-01-05 17:26:10 +0100

    a

--- app/Views/php/index.php
+++ app/Views/php/index.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="author" content="Mabule">
 	<link rel="stylesheet" type="text/css" href="/css/common.css">
+    <link rel="stylesheet" type="text/css" href="css/common.css">
 	<title>Accueil</title>
 </head>
 
